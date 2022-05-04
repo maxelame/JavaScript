4f74e3d9096dbd97672c5e5799984066c159a554
maxelame <maxelame31@gmail.com>
2022-05-04 23:05:21 +0300

    Implemented Json reading from the task 2 file.json using, for example, the fs module. for further use in the function described in the task

---
+++ HW_3_ADD_JS_task2(with stars).js
@@ -0,0 +1,13 @@
+// Task 2*** Реализуйте считывание JSONки из файла task2.json с помощью, например, 
+// модуля fs. для дальнейшего использования в функции, описанной в задании
+
+let jsonData = require('./task2.json'); 
+ 
+console.log(jsonData); 
+
+
+// const fs = require('fs'); 
+ 
+// let rawdata = fs.readFileSync('./task2.json'); 
+// let users = JSON.parse(rawdata); 
+// console.log(users); 
